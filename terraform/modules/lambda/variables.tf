6a178626db47d549332c3528c18f168984f000f3
maahambanu <maahambanu@gmail.com>
2026-05-08 16:04:04 +0500

    Adding ARN

--- terraform/modules/lambda/variables.tf
+++ terraform/modules/lambda/variables.tf
@@ -10,3 +10,8 @@ variable "dynamodb_table_arn" {
   description = "DynamoDB table ARN"
   type        = string
 }
+
+variable "api_gateway_execution_arn" {
+  type        = string
+  description = "Execution ARN of API Gateway"
+}
