c6c5b7b9dfd129d73cdc4dbcf8920899b7175674
maahambanu <maahambanu@gmail.com>
2026-05-08 22:04:59 +0500

    Changes in CI/CD cofiguration file

--- terraform/modules/lambda/variables.tf
+++ terraform/modules/lambda/variables.tf
@@ -23,3 +23,8 @@ variable "lambda_artifact_bucket" {
 variable "lambda_artifact_key" {
   type = string
 }
+
+variable "lambda_source_hash" {
+  description = "Base64 encoded SHA256 hash of Lambda artifact"
+  type        = string
+}
