5381aeb832a25276d9ac2c280b36a3c91dbc4d98
maahambanu <maahambanu@gmail.com>
2026-05-09 01:03:58 +0500

    Fixing bucket name issue

--- terraform/modules/s3-artifacts/main.tf
+++ terraform/modules/s3-artifacts/main.tf
@@ -54,7 +54,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
   rule {
     apply_server_side_encryption_by_default {
       kms_master_key_id = aws_kms_key.s3_kms.arn
-      sse_algorithm     = "aws:kms"
+      sse_algorithm     = "AES256"
     }
   }
 }
