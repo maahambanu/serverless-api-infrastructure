96482be9d3ea1205463f0a659b28cbcf935d55d1
maahambanu <maahambanu@gmail.com>
2026-05-09 11:11:47 +0500

    Clean up artifact deployment pipeline

--- terraform/modules/app/variables.tf
+++ terraform/modules/app/variables.tf
@@ -7,6 +7,10 @@ variable "lambda_source_hash" {
   type        = string
 }
 
+variable "lambda_artifact_bucket" {
+  type = string
+}
+
 variable "lambda_artifact_key" {
   description = "S3 object key for Lambda artifact"
   type        = string
